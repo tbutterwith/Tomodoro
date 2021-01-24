4eaa1497f54394f2a588a0602c7e4399635b362c
Tom Butterwith <thomas.butterwith@skyscanner.net>
2021-01-24 19:33:13 +0000

    Initial Commit

---
+++ Tomodoro/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Tomodoro
+//
+//  Created by Thomas Butterwith on 24/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
